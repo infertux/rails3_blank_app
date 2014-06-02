b8e06563b626c5dc770cb66fb61f95c28203756b
Cédric Félizard <cedric@felizard.fr>
2014-06-02 14:03:05 +0000

    Initial commit
    `rails _3.2.18_ new rails3_blank_app`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3BlankApp::Application.config.secret_token = '5df55bf4703d37df79f9cae89be9875d43fe7ca65580119b159c0938af1e7f4ed05428c2302d20ed227011362706257c659ce0e7e79d641f9b202f5097cab13f'
